68fadc2efedeea4d457d66e4b93b3d291920bf5b
Julien Barnier <julien.barnier@cnrs.fr>
2023-09-27 16:55:05 +0200

    fix: add snakemake install

--- mamba_create_env.sh
+++ mamba_create_env.sh
@@ -3,5 +3,5 @@
 mamba create -n dginn
 mamba activate dginn
 mamba install python=3.10
-mamba install -c bioconda biopython mafft emboss ete3 prank phyml treerecs bpp-core bpp-seq bpp-phyl bpp-popgen hyphy paml
+mamba install -c bioconda biopython mafft emboss ete3 prank phyml treerecs bpp-core bpp-seq bpp-phyl bpp-popgen hyphy paml snakemake
 mamba install pandas
